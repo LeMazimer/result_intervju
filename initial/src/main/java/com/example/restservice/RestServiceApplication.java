5ef20ae41c2cc4375a40a5c0a65992d6a58e84ca
LeMazimer <zizek.gaj@gmail.com>
2023-03-14 23:37:06 +0100

    working version 1

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,49 +1,101 @@
 package com.example.restservice;
 
-import java.util.concurrent.Executor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAsync
+@RestController
 public class RestServiceApplication {
-	public static int MAX_THREADS = 4;
-	public String[] RESULT_CONTENT = {};
+    private static final AtomicLong counter = new AtomicLong();
+    private final List<String> urls = Arrays.asList(
+        "https://www.result.si/projekti/",
+		"https://www.result.si/o-nas/",
+		"https://www.result.si/kariera/",
+		//"https://www.result.si/blog/"
+		"krneki"
+    );
 
-	@Bean
+    @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-	public String callApi(String url) {
-        ResponseEntity<String> response = this.restTemplate().getForEntity(url, String.class);
-        return response.getBody();
+    private String callApi(String url) {
+        return restTemplate().getForObject(url, String.class);
     }
 
-    @Bean
-    public Executor executor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(this.MAX_THREADS);
-        executor.setMaxPoolSize(this.MAX_THREADS);
-        executor.setQueueCapacity(500);
-        executor.initialize();
-        return executor;
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
-		/*ExecutorService pool = Executors.newFixedThreadPool(
-			Math.min(
-				RestServiceApplication.MAX_THREADS, Integer.parseInt(args[0])
-			)
-		);*/
-	}
+    @GetMapping("/contentscraper/{maxCalls}")
+    public ContentScraper callApiConcurrently(@PathVariable int maxCalls) {
+        ExecutorService executor = Executors.newFixedThreadPool(maxCalls);
+
+        List<CompletableFuture<String>> futures = urls.stream()
+            .map(url -> CompletableFuture.supplyAsync(() -> callApi(url), executor))
+            .collect(Collectors.toList());
+
+        //List<String> results = futures.stream()
+        //    .map(CompletableFuture::join)
+        //    .collect(Collectors.toList());
+		
+		List<String> results = futures.stream()
+			.map(future -> {
+				try {return future.join();} catch (Exception e) { return "";}
+			})
+			.collect(Collectors.toList());
+
+        executor.shutdown();
+
+		int successfulCalls = 0;
+		int unsuccessfulCalls = 0;
+		
+		// prep default value
+		String[] titles = new String[results.size()];
+		Arrays.fill(titles, "NO_TITLE_FOUND");
+		
+		// html tag pattern
+        Pattern pattern = Pattern.compile("<title>(.*?)</title>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+		int i = 0;
+
+        for (String result : results) {
+            Matcher matcher = pattern.matcher(result);
+            if (matcher.find()) {
+                String title = matcher.group(1);
+                titles[i] = title;
+				successfulCalls ++;
+            }
+			else {
+				unsuccessfulCalls ++;
+			}
+			i ++;
+        }
+		
+        return new ContentScraper(
+			this.counter.incrementAndGet(),
+			titles,
+			successfulCalls,
+			unsuccessfulCalls,
+			successfulCalls + unsuccessfulCalls
+		);
+    }
+
 }
